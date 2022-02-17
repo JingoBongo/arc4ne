54831092c2d34ba0024074de4e1de4fa569ae872
JingoBongo <https://github.com/JingoBongo>
2022-02-17 18:00:54 +0200

    added presentation

--- main/example.py
+++ main/example.py
@@ -2,10 +2,10 @@ import arc4ne
 
 def main():
     alg = arc4ne.Arc4ne()
-    msg = 'very Importannnnnce 1232342342 message'
-    key = 'unlock me_123'
+    msg = 'some other text to prove it works'
+    key = 'unlock me_123 with an addition'
     alg.set_key(key)
-    alg.use_scrypt(False)
+    alg.use_scrypt(True)
 
     print('Msg: %s' % msg)
     print('Key: %s' % key)
@@ -15,7 +15,7 @@ def main():
 
     alg2 = arc4ne.Arc4ne()
     alg2.set_key(key)
-    alg2.use_scrypt(False)
+    alg2.use_scrypt(True)
 
     result = alg2.decrypt(cipher)
     print('Decoded: %s ' % result)
